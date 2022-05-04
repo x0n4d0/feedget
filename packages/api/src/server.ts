f53c08dd9ef8d6b9a43ba58dac7ab0f586b52166
x0n4d0 <romagnolimauricio@gmail.com>
2022-05-04 18:50:11 -0300

    feat: start send e-mail test using mailtrap

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -1,10 +1,20 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
 
 import { prisma } from './prisma';
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: process.env.NODEMAILER_USER,
+    pass: process.env.NODEMAILER_PASSWORD
+  }
+});
 
 app.post('/feedbacks', async (request, response) => {
   const { type, comment, screenshot } = request.body;
@@ -16,6 +26,18 @@ app.post('/feedbacks', async (request, response) => {
       screenshot
     }
   })
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Maurício Romagnoli <romagnolimauricio@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      '<div style="font-family: sans-serif; font-size: 16px; color: #111;">',
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      '</div>'
+    ].join('\n')
+  });
 
   return response.status(201).json({ data: createdFeedback });
 });
