5fd84473af81d059605f3fd406a88c5946699905
x0n4d0 <romagnolimauricio@gmail.com>
2022-05-04 18:34:00 -0300

    feat: start feedbacks route

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -1,6 +1,24 @@
 import express from 'express';
 
+import { prisma } from './prisma';
+
 const app = express();
+
+app.use(express.json());
+
+app.post('/feedbacks', async (request, response) => {
+  const { type, comment, screenshot } = request.body;
+
+  const createdFeedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot
+    }
+  })
+
+  return response.status(201).json({ data: createdFeedback });
+});
 
 app.listen(3333, () => {
   console.log('[*] Server running at 3333!');
