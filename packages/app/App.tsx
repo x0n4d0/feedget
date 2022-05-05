f348f16703048271afc125472ec30649c5fe7f98
x0n4d0 <romagnolimauricio@gmail.com>
2022-05-05 20:52:30 -0300

    feat: add fonts in App component

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -1,4 +1,6 @@
 import { View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 
 import { theme } from './src/theme';
 
@@ -6,6 +8,15 @@ import { Widget } from './src/components/widget';
 import { StatusBar } from './src/components/status-bar';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <View style={{
       flex: 1,
