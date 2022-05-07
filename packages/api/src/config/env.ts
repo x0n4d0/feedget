fa8a1631725b7383d31d3d144d5bc7f40b26c208
x0n4d0 <romagnolimauricio@gmail.com>
2022-05-07 01:55:59 -0300

    feat: set SMTP_PORT as Number in Env

--- packages/api/src/config/env.ts
+++ packages/api/src/config/env.ts
@@ -2,7 +2,7 @@ export const Env = {
   DATABASE_URL: process.env.DATABASE_URL,
   PORT: Number(process.env.PORT),
   SMTP_HOST: process.env.SMTP_HOST,
-  SMTP_PORT: process.env.SMTP_PORT,
+  SMTP_PORT: Number(process.env.SMTP_PORT),
   SMTP_USER: process.env.SMTP_USER,
   SMTP_PASSWORD: process.env.SMTP_PASSWORD
 }
