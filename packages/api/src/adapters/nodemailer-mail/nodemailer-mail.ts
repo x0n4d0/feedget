ab2e3adea060ee58a8a0005fc22639e9d37e993d
x0n4d0 <romagnolimauricio@gmail.com>
2022-05-07 01:20:18 -0300

    feat: add Environments variables

--- packages/api/src/adapters/nodemailer-mail/nodemailer-mail.ts
+++ packages/api/src/adapters/nodemailer-mail/nodemailer-mail.ts
@@ -1,13 +1,14 @@
 import nodemailer from 'nodemailer';
 
+import { Env } from './../../config/env';
 import { MailAdapter, SendMailData } from './../mail';
 
 const transport = nodemailer.createTransport({
-  host: "smtp.mailtrap.io",
-  port: 2525,
+  host: Env.SMTP_HOST,
+  port: Number(Env.SMTP_PORT),
   auth: {
-    user: process.env.NODEMAILER_USER,
-    pass: process.env.NODEMAILER_PASSWORD
+    user: Env.SMTP_USER,
+    pass: Env.SMTP_PASSWORD
   }
 });
 
---
+++ packages/api/src/config/env.ts
@@ -0,0 +1,7 @@
+export const Env = {
+  DATABASE_URL: process.env.DATABASE_URL,
+  SMTP_HOST: process.env.SMTP_HOST,
+  SMTP_PORT: process.env.SMTP_PORT,
+  SMTP_USER: process.env.SMTP_USER,
+  SMTP_PASSWORD: process.env.SMTP_PASSWORD
+}
